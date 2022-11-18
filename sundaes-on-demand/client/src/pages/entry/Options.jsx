b01062d29922de02b58bb8b038669a2bf2a9a946
BogdanNitica99 <bogdan.nitica@yahoo.com>
2022-11-18 16:40:12 +0200

    Add alert banner for server error response and solve race condition on async

--- sundaes-on-demand/client/src/pages/entry/Options.jsx
+++ sundaes-on-demand/client/src/pages/entry/Options.jsx
@@ -1,18 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const OptionComponent =
     optionType === "scoop" ? ScoopOptions : ToppingOptions;
---
+++ sundaes-on-demand/client/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,22 @@
+import { screen, render, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
+import OrderEntry from "../OrderEntry";
+
+test("shows error from server", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:3030/scoop", (req, res, ctx) =>
+      res(ctx.status(500))
+    ),
+    rest.get("http://localhost:3030/topping", (req, res, ctx) =>
+      res(ctx.status(500))
+    )
+  );
+
+  render(<OrderEntry />);
+
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
+  });
+});
